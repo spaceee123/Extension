f9acaf6232502017706956c0a10e349fef785416
budarina <lena.raytkovskaya@gmail.com>
2023-05-12 12:24:53 +0300

    цйу

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,46 @@
-
+var jsonp = require('jsonp')
+var tabInfo
+var auth_token
 function submitForm() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    fetch('https://login.carinspect.org/login')
-    console.log(username, password)
-}
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('loginBtn').addEventListener('click', submitForm);
-
-    let version = chrome.runtime.getManifest().version;
-    console.log(version);
+    // let version = chrome.runtime.getManifest().version;
+    // console.log(version);
     let userAgent = navigator.userAgent;
     console.log(userAgent);
 
-    const json =JSON.stringify({
-        version: version,
+    const json = JSON.stringify({
         userAgent: userAgent,
+        redirect: 'https://login.carinspect.org/login'
     })
 
-    const encodedJson = base64.encode(json)
-    console.log(encodedJson)
-       chrome.tabs.create({ url: `https://login.carinspect.org/login?params=${encodedJson}` });
+    const encodedJson =  window.btoa(json)
+
+    chrome.tabs.create({ url: `https://login.carinspect.org/login?app_extension=1&params=${encodedJson}` }, (tab) =>{
+        document.body.innerHTML += 'tabid = ' + tab.id
+        tabInfo = tab
+    });
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginBtn').addEventListener('click', submitForm);
 });
+
+
+if (window.location.href.includes('login.carinspect.org/login?app_extension=1') && document.getElementById('auth_token') !== null && document.getElementById('auth_token').value != '') {
+    alert(document.getElementById('auth_token').value)
+    auth_token = document.getElementById('auth_token').value
+
+    fetchUserInfo(document.getElementById('auth_token').value).then(data => alert(data))
+}
+
+
+
+async function fetchUserInfo(auth_token){
+    const res = await fetch("https://api.carinspect.org/user", {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + auth_token
+        }
+    })
+
+    return await res.json()
+}
